2ba96cce1369d069fd7542892b64cfc8834da0fa
Adithya Battula <battulaadhi509@gmail.com>
2026-06-14 08:24:47 +0530

    add prediction cache service

---
+++ backend/src/services/predictionCache.js
@@ -0,0 +1,15 @@
+const cache = new Map();
+
+module.exports = {
+  get(key) {
+    return cache.get(key);
+  },
+
+  set(key, value) {
+    cache.set(key, value);
+  },
+
+  clear() {
+    cache.clear();
+  }
+};
